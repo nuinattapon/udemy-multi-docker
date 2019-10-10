45e6d0a3825ecdd0ce132c6555e7ef1623792580
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2019-10-10 22:53:59 +0700

    OK

--- api/index.js
+++ api/index.js
@@ -42,11 +42,13 @@ app.get("/", (req, res) => {
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * FROM values")
   res.send(values.rows)
+  console.log(values.rows)
 })
 
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
     res.send(values)
+    console.log(values)
   })
 })
 
